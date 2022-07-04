03ada4cffbb1f43ae3a7f7182dcf5c5a62049588
Rafal <mosakowski.r@gmail.com>
2022-07-04 20:08:25 +0200

    added UI and DOM to the game

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-const randomArr = ['Rock', 'Paper', 'Scissors'];
+const randomArr = ['rock', 'paper', 'scissors'];
 
 function computerPlay() {
     return randomArr[Math.floor(Math.random()*randomArr.length)]
@@ -8,31 +8,59 @@ function computerPlay() {
 
 let computerPoints = 0;
 let playerPoints = 0;
-let score = '0 - 0';
+let score = `${playerPoints} - ${computerPoints}`;
+
+let scoreDiv = document.querySelector('.result')
+scoreDiv.innerHTML = score;
+
+let msgDiv = document.querySelector('.msg');
+msgDiv.innerHTML = 'Choose wisely!'
+
+const restartDiv = document.querySelector('.restart')
+restartDiv.addEventListener('click', () => {
+    computerPoints = 0;
+    playerPoints = 0;
+    msgDiv.innerHTML = 'Game restarted!';
+    score = `${playerPoints} - ${computerPoints}`
+    scoreDiv.innerHTML = score;
+})
+
+const playerChoices = document.querySelectorAll('.item')
+let playerChoice
+
+playerChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    playerChoice = e.target.closest('.item').classList[1];
+    if (computerPoints < 5 && playerPoints < 5) {
+        playRound();
+    } else {
+        scoreDiv.innerHTML = 'Restart yor game!'
+    }
+}))
 
 function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay().toLowerCase();
-    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerPlay();
+    playerSelection = playerChoice;
 
     if (playerSelection === computerSelection) {
-        console.log(score)
-        return 'It\'s a tie! Score is ' + score;
+        msgDiv.innerHTML = 'It\'s a tie! Score is';
+        scoreDiv.innerHTML = score;
+
     }
     else if(playerSelection !== computerSelection) {
         if (playerSelection === 'rock' && computerSelection === 'paper' || 
         playerSelection === 'paper' && computerSelection === 'scissors' || 
         playerSelection === 'scissors' && computerSelection === 'rock') {
             computerPoints += 1;
-            score = playerPoints + ' - ' + computerPoints;
+            score = `${playerPoints} - ${computerPoints}`;
             if (computerPoints < 5) {
-                console.log(score);
-                return 'Computer wins. Score is ' + score;
+                msgDiv.innerHTML = 'Computer wins. Score is';
+                scoreDiv.innerHTML = score;
+            } else if (computerPoints === 5){
+                msgDiv.innerHTML = "It's Over. Computer WINS";
+                scoreDiv.innerHTML = score;
             } else {
-                computerPoints = 0;
-                playerPoints = 0;
-                oldScore = score;
-                score = playerPoints + ' - ' + computerPoints;
-                return "It's Over. Computer WINS " + oldScore;
+                msgDiv.innerHTML = "It's Over. Computer WINS";
+                scoreDiv.innerHTML = 'Restart your game!';
             }
             
         } else if (playerSelection === 'rock' && computerSelection === 'scissors' || 
@@ -41,18 +69,22 @@ function playRound(playerSelection, computerSelection) {
             playerPoints += 1;
             score = playerPoints + ' - ' + computerPoints;
             if (playerPoints < 5) {
-                console.log(score)
-                return 'You win. Score is ' + score;
+                msgDiv.innerHTML = 'You win. Score is';
+                scoreDiv.innerHTML = score;
+            } else if (playerPoints === 5){
+                msgDiv.innerHTML = "GREAT GAME. YOU WIN";
+                scoreDiv.innerHTML = score;
             } else {
-                computerPoints = 0;
-                playerPoints = 0;
-                oldScore = score;
-                score = playerPoints + ' - ' + computerPoints;
-                return  "GREAT GAME. YOU WIN " + oldScore;
+                msgDiv.innerHTML = "GREAT GAME. YOU WIN";
+                scoreDiv.innerHTML = 'Restart your game!';
             }
             
         }
     }
 }
+
+
+
+
 
 
